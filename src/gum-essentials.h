48a486f130c63f6beffd635e966dcf2b73798c1d
Gumse <gumse@gumse.de>
2023-02-17 19:13:57 +0100

    Fr 17. Feb 19:13:57 CET 2023

--- src/gum-essentials.h
+++ src/gum-essentials.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Essentials/SmoothFloat.h"
-#include "Essentials/Compression.h"
 #include "Essentials/Settings.h"
 #include "Essentials/Tools.h"
 #include "Essentials/FPS.h"
